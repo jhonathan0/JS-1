9c654f728c94c643cbbe09caa1b883d1dda55177
jhonathan0 <147885506+jhonathan0@users.noreply.github.com>
2024-09-15 17:38:13 -0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-document.querySelector('.mudarTextoBotao').addEventListener('click', function() {
-    const paragrafo = document.querySelector('.descricao');
-    paragrafo.texteContent = 'Texto alterado!';
-});
+
 
 
 document.querySelector('.destacarBotao').addEventListener('click', function() {

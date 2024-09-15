7e078376713fcab58f2030748f4cc09d94812cb7
jhonathan0 <147885506+jhonathan0@users.noreply.github.com>
2024-09-15 17:01:00 -0300

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector('.mudarTextoBotao').addEventListener('click', function() {
+    const paragrafo = document.querySelector('.descricao');
+    paragrafo.texteContent = 'Texto alterado!';
+});
+
+
+document.querySelector('.destacarBotao').addEventListener('click', function() {
+    const titulo = document.querySelector('.titulo');
+    titulo.classList.add('destaque');
+});

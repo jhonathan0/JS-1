73d0f7b095ef05b0996d03dd2e253bb8bebc2378
jhonathan0 <147885506+jhonathan0@users.noreply.github.com>
2024-09-15 17:56:39 -0300

    Alteração no js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.querySelector('.mudarTextoBotao').addEventListener('click', function() 
 });
 
 
+
 document.querySelector('.destacarBotao').addEventListener('click', function() {
     const titulo = document.querySelector('.titulo');
     titulo.classList.add('destaque');
